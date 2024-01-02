280b0f90feda16b0be7496c98dffe74c70323c1f
Eduardo Higareda <eldelentes@gmail.com>
2024-01-02 15:27:52 -0600

    add github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://briqmx.github.io',
+  base: '/careers',
   integrations: [tailwind()]
 });
